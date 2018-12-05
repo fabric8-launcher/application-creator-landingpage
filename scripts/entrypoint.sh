1c30ffd0acb6b699b4f6cbea677a4f70c6fe50ec
George Gastaldi <gegastaldi@gmail.com>
2018-12-05 07:53:31 -0200

    chore: encode single quotes

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,7 +3,8 @@
 CONFIG_TEMPLATE="/usr/bin/config-tmpl.json"
 INDEX="/usr/share/nginx/html/index.html"
 
-export WELCOME_APP_CONFIG_ENCODED = echo ${WELCOME_APP_CONFIG} | tr -d '[:space:]' | sed 's/\"/\\\"/g'
+export WELCOME_APP_CONFIG_ENCODED = echo ${WELCOME_APP_CONFIG} | tr -d '[:space:]' | sed 's/\"/\\\"/g' | sed s/\'/\\\'/g
+
 # create injected index.html with json settings
 sed -i -e "s/<script id=\"script-injection\"><\/script>/<script id=\"injected-script\">$(envsubst < ${CONFIG_TEMPLATE} | tr -d '[:space:]' | sed -e 's/[]\/$*.^[]/\\&/g')<\/script>/g" ${INDEX}
 echo -------------------------------------
