3b3aad6af15b5211a15079160f6ad05020d96815
George Gastaldi <gegastaldi@gmail.com>
2018-12-05 07:08:13 -0200

    fix: sed sucks

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -3,11 +3,8 @@
 CONFIG_TEMPLATE="/usr/bin/config-tmpl.json"
 INDEX="/usr/share/nginx/html/index.html"
 
-# process and export json settings from given template (escape double quotes, remove spaces and line breaks)
-export JSON_CONFIG="$(envsubst < ${CONFIG_TEMPLATE} | tr -d '[:space:]' | sed 's/\"/\\\"/g')"
-
 # create injected index.html with json settings
-sed -i -e "s/<script id=\"script-injection\"><\/script>/<script id=\"injected-script\">$(echo $JSON_CONFIG | sed -e 's/[]\/$*.^[]/\\&/g')<\/script>/g" ${INDEX}
+sed -i -e "s/<script id=\"script-injection\"><\/script>/<script id=\"injected-script\">$(envsubst < ${CONFIG_TEMPLATE} | tr -d '[:space:]' | sed -e 's/[]\/$*.^[]/\\&/g')<\/script>/g" ${INDEX}
 echo -------------------------------------
 cat ${INDEX}
 
